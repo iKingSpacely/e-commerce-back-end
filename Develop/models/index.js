f132cfc03d69c3b979639bd76d260bdd6af926ee
Spacely <accolin89@gmail.c>
2023-06-26 17:06:24 -0500

    setting up the models tables, one to one, one to many, and index file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignkey: 'category_id'
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignkey: 'product_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'prod_tag'
+}),
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'many_tags'
+}),
 
 module.exports = {
   Product,

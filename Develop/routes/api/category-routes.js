97830302796b66af96e443f5a7a03bb2a39c4572
Spacely <accolin89@gmail.c>
2023-06-26 20:01:28 -0500

    working through category-routes get by ID and create a new category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -14,20 +14,39 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const oneCat = await Category.findByPk(req.params.id, {
+      include: [{ model: Category, through: Product, as: 'category_id' }]
+    });
+
+    if (!oneCat) {
+      res.status(404).json({ message: 'Cannot find category ID!' });
+      return;
+    }
+    res.status(200).json(oneCat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const createCat = await Category.create(req.body);
+    res.status(200).json(createCat);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 

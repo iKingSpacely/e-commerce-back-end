392af60158d377228b9371b659050aacc3194f29
Spacely <accolin89@gmail.c>
2023-06-26 20:11:25 -0500

    finalizing the delete and update on category routes file

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -38,16 +38,46 @@ router.post('/', async (req, res) => {
     const createCat = await Category.create(req.body);
     res.status(200).json(createCat);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCat = await Category.update({
+      where: {
+        id: req.params.id
+      }
+    });
+    
+    if (!updateCat) {
+      res.status(404).json({ message: 'Cannot find category ID!'});
+      return;
+    }
+    res.status(200).json(updateCat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCat = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!deleteCat) {
+      res.status(404).json({ message: 'Cannot find category ID!' });
+      return;
+    }
+    res.status(200).json(deleteCat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
